b2dd3a1dede3e1d71e9699f8528315b2db915df0
Raghuveersingh1436 <145828543+Raghuveersingh1436@users.noreply.github.com>
2023-12-05 19:46:18 +0530

    Update resource.tf

--- resource.tf
+++ resource.tf
@@ -59,8 +59,8 @@ resource "aws_security_group" "pub-sg" {
   }
   #httpd inbound rule
   ingress {
-    from_port = 888
-    to_port = 888
+    from_port = 80
+    to_port = 80
     protocol = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
